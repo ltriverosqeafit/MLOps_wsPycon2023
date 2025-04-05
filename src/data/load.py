65c4ba6336abe462fb59a6b8fc5ddd85f9752806
ltriverosqeafit <ltriverosq@eafit.edu.co>
2025-04-05 11:09:42 -0500

    Update load.py
    prueba_commit

--- src/data/load.py
+++ src/data/load.py
@@ -5,6 +5,7 @@ from torch.utils.data import TensorDataset
 import argparse
 import wandb
 
+#prueba commit
 parser = argparse.ArgumentParser()
 parser.add_argument('--IdExecution', type=str, help='ID of the execution')
 args = parser.parse_args()
